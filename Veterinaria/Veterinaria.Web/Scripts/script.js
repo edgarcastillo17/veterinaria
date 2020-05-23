d208c9e48477d9e6053bdae8b289c7e6ace12a99
edgarcastillo17 <edgar.castillo17@tectijuana.edu.mx>
2020-05-23 16:49:48 -0700

    P05_Vistas

---
+++ Veterinaria/Veterinaria.Web/Scripts/script.js
@@ -0,0 +1,10 @@
+﻿// Initialize and add the map
+function initMap() {
+    // The location
+    var itt = { lat: 32.529134, lng: -116.986848 };
+    // The map, centered
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 4, center: itt });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({ position: itt, map: map });
+}
