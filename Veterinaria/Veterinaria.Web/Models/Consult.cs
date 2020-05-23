7d9867da66984321d7342cf5a76757b28768273d
edgarcastillo17 <edgar.castillo17@tectijuana.edu.mx>
2020-05-22 18:31:41 -0700

    ProyectoInicial_Veterinaria

---
+++ Veterinaria/Veterinaria.Web/Models/Consult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Veterinaria.Web.Models
+{
+    public class Consult
+    {
+    }
+}
---
+++ Veterinaria/Veterinaria.Web/Models/Owner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Veterinaria.Web.Models
+{
+    public class Owner
+    {
+        public int MyProperty { get; set; }
+        [Display(Name = "Nombre")]
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Display(Name = "Apellidos")]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [Display(Name = "Dirección")]
+        [MaxLength(400)]
+        public string Address { get; set; }
+        [Display(Name = "Teléfono")]
+        [Required]
+        [MaxLength(20)]
+        public string Phone { get; set; }
+        [Display(Name = "Correo")]
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+    }
+}
